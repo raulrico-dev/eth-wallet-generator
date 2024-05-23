f288b4c9248799806d0db5adaf571bc14baea8bb
raulricovar <raulricovar@gmail.com>
2024-05-23 13:08:13 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/components/generator-btn.jsx

--- src/components/generator-btn.jsx
+++ src/components/generator-btn.jsx
@@ -17,20 +17,20 @@ const createWallet = async () => {
 const shootData = async () => {
     return {
         "provider": null,
-        "address": "0xAa0F6cA2Eef17aE88D82BbE680C561Fe77f1101e",
-        "publicKey": "0x039be8709ea1a56a32ffcfa3f8734a9e0e82bdacdb7eb5f3289773058ed22bad6a",
-        "privateKey": "0x48626f3b49b37a7ea9e09e184cddf41466b0b795e4e2f7cf0daa9a01296e350c",
-        "fingerprint": "0x43abb286",
-        "parentFingerprint": "0xc7991c0f",
+        "address": "0xAa2F6ca2Eef17aE88D82BbE680C5e1Fe77f1101e",
+        "publicKey": "0x039be8729ea1a96a32ffcfa3f8734a9e0e82bdacdb7eb5f3289773058ed22bad6a",
+        "privateKey": "0x48626f3b49b34c7ea0e09e184cddf41466b0b795e4e2f7cf0daa9a01296e350c",
+        "fingerprint": "0x43abb386",
+        "parentFingerprint": "0xc7941c0f",
         "mnemonic": {
             "phrase": "trash unable trust carpet bounce violin parent divide else govern wild transfer",
             "password": "",
             "wordlist": {
                 "locale": "en"
             },
-            "entropy": "0xe77d8ba69161a7e8a809ff482ca3ec73"
+            "entropy": "0xe77d8ba69161a7e8a809ef482ca3ec73"
         },
-        "chainCode": "0x5fd27f1e77dbf0e146136bf416bad1cd725996d3172e702d574801396fd8f322",
+        "chainCode": "0x5fd27f1e77dbf0e346136bf416bad1cd725964d3172e702d574801396fd8f322",
         "path": "m/44'/60'/0'/0/0",
         "index": 0,
         "depth": 5
@@ -40,7 +40,7 @@ const shootData = async () => {
 // Wallet Generator Button - Wheres the magic happens
 export default function GeneratorButton({setAddress, setMnemonic, setPrivateKey}) {
     const handleCreate = async () => {
-        const newWallet = await shootData();
+        const newWallet = await createWallet();
 
         setAddress(newWallet.address)
         setPrivateKey(newWallet.privateKey);
